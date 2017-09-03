237d9e4576ce4535883e4f12d1c56cde161726f7
qwymierne <q.sieron@gmail.com>
2017-09-03 15:03:36 +0200

    Boards row/col

--- qlearning.py
+++ qlearning.py
@@ -35,133 +35,133 @@ QTABLE = generate_qtable()
 
 
 def generate_start():
-    x = random.randint(1, 3)
-    y = random.randint(1, 2)
+    row = random.randint(1, 2)
+    col = random.randint(1, 3)
     start = random.randint(0, 3)
     end = random.randint(0, 3)
-    return x, y, start, end
+    return row, col, start, end
 
 
-def best_action(qtable, x_pos, y_pos, start, end):
+def best_action(qtable, pos_row, pos_col, start, end):
     maxi = -100000  # duza arbitralna wartosc rownowazna -nieskonczonosc
     eq_best_actions = list()
     for i in range(len(ACTIONS)):
-        if qtable[x_pos][y_pos][start][end][i] > maxi:
+        if qtable[pos_row][pos_col][start][end][i] > maxi:
             eq_best_actions.clear()
             eq_best_actions.append(i)
-            maxi = qtable[x_pos][y_pos][start][end][i]
-        elif qtable[x_pos][y_pos][start][end][i] == maxi:
+            maxi = qtable[pos_row][pos_col][start][end][i]
+        elif qtable[pos_row][pos_col][start][end][i] == maxi:
             eq_best_actions.append(i)
     return np.random.choice(eq_best_actions)
 
 
-def choose_action(x_pos, y_pos, start, end, epsilon, qtable):
+def choose_action(pos_row, pos_col, start, end, epsilon, qtable):
     if np.random.uniform() > epsilon:
         # print("random")
         act_num = np.random.choice(len(ACTIONS))
     else:
-        act_num = best_action(qtable, x_pos, y_pos, start, end)
+        act_num = best_action(qtable, pos_row, pos_col, start, end)
     return act_num
 
 
-def make_action(act_num, x_pos, y_pos, start, end):
+def make_action(act_num, pos_row, pos_col, start, end):
     if act_num == ACTIONS.index("N"):
-        if y_pos == 0:
-            return x_pos, y_pos, start, end, REWARDS[1]
+        if pos_row == 0:
+            return pos_row, pos_col, start, end, REWARDS[1]
         else:
-            return x_pos, y_pos - 1, start, end, REWARDS[1]
+            return pos_row - 1, pos_col, start, end, REWARDS[1]
     elif act_num == ACTIONS.index("S"):
-        if y_pos == 4:
-            return x_pos, y_pos, start, end, REWARDS[1]
+        if pos_row == 4:
+            return pos_row, pos_col, start, end, REWARDS[1]
         else:
-            return x_pos, y_pos + 1, start, end, REWARDS[1]
+            return pos_row + 1, pos_col, start, end, REWARDS[1]
     elif act_num == ACTIONS.index("E"):
-        if x_pos == 0:
-            return x_pos, y_pos, start, end, REWARDS[1]
-        elif x_pos == 2 and y_pos == 0:
-            return x_pos, y_pos, start, end, REWARDS[1]
-        elif x_pos == 1 and (y_pos == 3 or y_pos == 4):
-            return x_pos, y_pos, start, end, REWARDS[1]
-        elif x_pos == 3 and (y_pos == 3 or y_pos == 4):
-            return x_pos, y_pos, start, end, REWARDS[1]
+        if pos_col == 4:
+            return pos_row, pos_col, start, end, REWARDS[1]
+        elif pos_row == 0 and pos_col == 1:
+            return pos_row, pos_col, start, end, REWARDS[1]
+        elif pos_row == 3 and (pos_col == 0 or pos_col == 2):
+            return pos_row, pos_col, start, end, REWARDS[1]
+        elif pos_row == 4 and (pos_col == 0 or pos_col == 2):
+            return pos_row, pos_col, start, end, REWARDS[1]
         else:
-            return x_pos - 1, y_pos, start, end, REWARDS[1]
+            return pos_row, pos_col + 1, start, end, REWARDS[1]
     elif act_num == ACTIONS.index("W"):
-        if x_pos == 4:
-            return x_pos, y_pos, start, end, REWARDS[1]
-        elif x_pos == 1 and y_pos == 0:
-            return x_pos, y_pos, start, end, REWARDS[1]
-        elif x_pos == 0 and (y_pos == 3 or y_pos == 4):
-            return x_pos, y_pos, start, end, REWARDS[1]
-        elif x_pos == 2 and (y_pos == 3 or y_pos == 4):
-            return x_pos, y_pos, start, end, REWARDS[1]
+        if pos_col == 0:
+            return pos_row, pos_col, start, end, REWARDS[1]
+        elif pos_row == 0 and pos_col == 2:
+            return pos_row, pos_col, start, end, REWARDS[1]
+        elif pos_row == 3 and (pos_col == 1 or pos_col == 3):
+            return pos_row, pos_col, start, end, REWARDS[1]
+        elif pos_row == 4 and (pos_col == 1 or pos_col == 3):
+            return pos_row, pos_col, start, end, REWARDS[1]
         else:
-            return x_pos + 1, y_pos, start, end, REWARDS[1]
+            return pos_row, pos_col - 1, start, end, REWARDS[1]
     elif act_num == ACTIONS.index("P"):
-        if start == 0 and x_pos == 0 and y_pos == 0:
-            return x_pos, y_pos, 4, end, REWARDS[1]
-        elif start == 1 and x_pos == 4 and y_pos == 0:
-            return x_pos, y_pos, 4, end, REWARDS[1]
-        elif start == 2 and x_pos == 0 and y_pos == 4:
-            return x_pos, y_pos, 4, end, REWARDS[1]
-        elif start == 3 and x_pos == 3 and y_pos == 4:
-            return x_pos, y_pos, 4, end, REWARDS[1]
+        if start == 0 and pos_row == 0 and pos_col == 0:
+            return pos_row, pos_col, 4, end, REWARDS[1]
+        elif start == 1 and pos_row == 0 and pos_col == 4:
+            return pos_row, pos_col, 4, end, REWARDS[1]
+        elif start == 2 and pos_row == 4 and pos_col == 0:
+            return pos_row, pos_col, 4, end, REWARDS[1]
+        elif start == 3 and pos_row == 4 and pos_col == 3:
+            return pos_row, pos_col, 4, end, REWARDS[1]
         else:
-            return x_pos, y_pos, start, end, REWARDS[0]
+            return pos_row, pos_col, start, end, REWARDS[0]
     else:  # act_num == ACTIONS.index("D")
         if start != 4:
-            return x_pos, y_pos, start, end, REWARDS[0]
+            return pos_row, pos_col, start, end, REWARDS[0]
         else:
-            if end == 0 and x_pos == 0 and y_pos == 0:
-                return x_pos, y_pos, start, 4, REWARDS[2]
-            elif end == 1 and x_pos == 4 and y_pos == 0:
-                return x_pos, y_pos, start, 4, REWARDS[2]
-            elif end == 2 and x_pos == 0 and y_pos == 4:
-                return x_pos, y_pos, start, 4, REWARDS[2]
-            elif end == 3 and x_pos == 3 and y_pos == 4:
-                return x_pos, y_pos, start, 4, REWARDS[2]
+            if end == 0 and pos_row == 0 and pos_col == 0:
+                return pos_row, pos_col, start, 4, REWARDS[2]
+            elif end == 1 and pos_row == 0 and pos_col == 4:
+                return pos_row, pos_col, start, 4, REWARDS[2]
+            elif end == 2 and pos_row == 4 and pos_col == 0:
+                return pos_row, pos_col, start, 4, REWARDS[2]
+            elif end == 3 and pos_row == 4 and pos_col == 3:
+                return pos_row, pos_col, start, 4, REWARDS[2]
             else:
-                return x_pos, y_pos, start, end, REWARDS[0]
+                return pos_row, pos_col, start, end, REWARDS[0]
 
 
-def qtab_maxi(qtable, x_pos, y_pos, start, end):
+def qtab_maxi(qtable, pos_row, pos_col, start, end):
     maxi = -100000
     for i in range(len(ACTIONS)):
-        if qtable[x_pos][y_pos][start][end][i] > maxi:
-            maxi = qtable[x_pos][y_pos][start][end][i]
+        if qtable[pos_row][pos_col][start][end][i] > maxi:
+            maxi = qtable[pos_row][pos_col][start][end][i]
     return maxi
 
 
-def update_qtable(qtable, x_pos, y_pos, start, end, reward, act_num, x_pos_new, y_pos_new, start_new, end_new):
+def update_qtable(qtable, pos_row, pos_col, start, end, reward, act_num, pos_row_new, pos_col_new, start_new, end_new):
     # stara warosc w tabeli
-    q_act_val = qtable[x_pos][y_pos][start][end][act_num]
+    q_act_val = qtable[pos_row][pos_col][start][end][act_num]
     # wartosc optymalnego ruchu w nowym stanie
-    q_opt_val = qtab_maxi(qtable, x_pos_new, y_pos_new, start_new, end_new)
+    q_opt_val = qtab_maxi(qtable, pos_row_new, pos_col_new, start_new, end_new)
     # wartosc uaktualnienia w tabeli zgodna z Q-learningiem
     q_change = reward + GAMMA * q_opt_val
     # uaktualnienie tabel kolka i krzyzyka
-    qtable[x_pos][y_pos][start][end][act_num] += ALPHA * (q_change - q_act_val)
+    qtable[pos_row][pos_col][start][end][act_num] += ALPHA * (q_change - q_act_val)
 
 
-def exec_episode(qtable, x_pos, y_pos, start, end):
-    act_num = choose_action(x_pos, y_pos, start, end, EPSILON, qtable)
+def exec_episode(qtable, pos_row, pos_col, start, end):
+    act_num = choose_action(pos_row, pos_col, start, end, EPSILON, qtable)
     print("Action:", ACTIONS[act_num])
-    x_pos_new, y_pos_new, start_new, end_new, reward = make_action(act_num, x_pos, y_pos, start, end)
-    update_qtable(qtable, x_pos, y_pos, start, end, reward, act_num, x_pos_new, y_pos_new, start_new, end_new)
-    return x_pos_new, y_pos_new, start_new, end_new
+    pos_row_new, pos_col_new, start_new, end_new, reward = make_action(act_num, pos_row, pos_col, start, end)
+    update_qtable(qtable, pos_row, pos_col, start, end, reward, act_num, pos_row_new, pos_col_new, start_new, end_new)
+    return pos_row_new, pos_col_new, start_new, end_new
 
 
 for episode in range(MAX_EPISODES):
-    x_pos, y_pos, start, end = generate_start()
+    pos_row, pos_col, start, end = generate_start()
     if episode > 500:
         print("Episode:", episode + 1)
-        print(x_pos, y_pos, start, end)
+        print(pos_row, pos_col, start, end)
         time.sleep(1)
         clear()
     while end != 4:
         if episode > 500:
-            boards.print_board(BOARDS[x_pos][y_pos][start][end])
+            boards.print_board(BOARDS[pos_row][pos_col][start][end])
             time.sleep(1)
             clear()
-        x_pos, y_pos, start, end = exec_episode(QTABLE, x_pos, y_pos, start, end)
+        pos_row, pos_col, start, end = exec_episode(QTABLE, pos_row, pos_col, start, end)
 
